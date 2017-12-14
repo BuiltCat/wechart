df05f09e66b2965e03af0f495608f9bb2519cc62
小半 <wt920116625@live.com>
2017-12-14 17:02:49 +0800

    shujuku

--- wechart_upload.sql
+++ wechart_upload.sql
@@ -10,10 +10,19 @@ Target Server Type    : MYSQL
 Target Server Version : 50719
 File Encoding         : 65001
 
-Date: 2017-12-09 11:33:58
+Date: 2017-12-14 17:02:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `userid` varchar(20) DEFAULT NULL,
+  `userpw` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for file
@@ -25,24 +34,9 @@ CREATE TABLE `file` (
   `filepath` varchar(255) DEFAULT NULL,
   `filename` varchar(255) DEFAULT NULL,
   `update` datetime DEFAULT NULL,
+  `class` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`fileid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of file
--- ----------------------------
-INSERT INTO `file` VALUES ('20171209014955', '155042242', 'public\\uploads//155042242', 'logo.png', '2017-12-09 01:49:55');
-INSERT INTO `file` VALUES ('20171209015556', '155042242', 'public\\uploads//155042242', '123.png', '2017-12-09 01:55:56');
-INSERT INTO `file` VALUES ('20171209015958', '155042242', 'public\\/155042242', '456.png', '2017-12-09 01:59:58');
-INSERT INTO `file` VALUES ('20171209021754', '155042242', 'public\\/155042242', 'logo1.png', '2017-12-09 02:17:54');
-INSERT INTO `file` VALUES ('20171209021840', '155042242', 'public\\/155042242', 'logo2.png', '2017-12-09 02:18:40');
-INSERT INTO `file` VALUES ('20171209022038', '155042242', 'public\\/155042242', 'logo3.png', '2017-12-09 02:20:38');
-INSERT INTO `file` VALUES ('20171209022513', '155042242', 'public\\uploads//155042242', 'logo4.png', '2017-12-09 02:25:13');
-INSERT INTO `file` VALUES ('20171209022527', '155042242', 'public\\uploads//155042242', 'logo5.png', '2017-12-09 02:25:27');
-INSERT INTO `file` VALUES ('20171209022649', '155042242', 'public\\uploads//155042242', 'logo6.png', '2017-12-09 02:26:49');
-INSERT INTO `file` VALUES ('20171209022817', '155042242', 'public\\uploads//155042242', 'logo7.png', '2017-12-09 02:28:17');
-INSERT INTO `file` VALUES ('20171209022939', '155042242', 'public\\uploads//155042242', 'logo8.png', '2017-12-09 02:29:39');
-INSERT INTO `file` VALUES ('20171209023215', '155042242', 'public\\uploads//155042242', 'logo9.png', '2017-12-09 02:32:15');
 
 -- ----------------------------
 -- Table structure for stuclass
@@ -52,13 +46,6 @@ CREATE TABLE `stuclass` (
   `userid` varchar(9) DEFAULT NULL,
   `userclass` varchar(20) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of stuclass
--- ----------------------------
-INSERT INTO `stuclass` VALUES ('155042242', '155042242计算机科学与技术');
-INSERT INTO `stuclass` VALUES ('155042242', '1550422计算机科学与技术');
-INSERT INTO `stuclass` VALUES ('155042242', '155042243计算机科学与技术');
 
 -- ----------------------------
 -- Table structure for teacher
@@ -72,11 +59,6 @@ CREATE TABLE `teacher` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of teacher
--- ----------------------------
-INSERT INTO `teacher` VALUES ('wangli', '123456', '王莉');
-
--- ----------------------------
 -- Table structure for teaclass
 -- ----------------------------
 DROP TABLE IF EXISTS `teaclass`;
@@ -86,12 +68,6 @@ CREATE TABLE `teaclass` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of teaclass
--- ----------------------------
-INSERT INTO `teaclass` VALUES ('wangli', '1550422计算机科学与技术');
-INSERT INTO `teaclass` VALUES ('wangli', '155042242计算机科学与技术');
-
--- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
@@ -99,11 +75,5 @@ CREATE TABLE `user` (
   `userid` varchar(9) NOT NULL,
   `userpw` varchar(20) DEFAULT NULL,
   `usernm` varchar(10) DEFAULT NULL,
-  `sex` varchar(2) DEFAULT NULL,
   PRIMARY KEY (`userid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('155042242', '123456', '王钟浩', '男');
